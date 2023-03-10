090179201ab4895e8ce07c61a03a03fd12a501fa
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-10 19:28:29 +0000

    Changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,44 +2,61 @@
 from fabric.api import *
 from os.path import exists
 from datetime import datetime
+import os
 
 env.hosts = ['18.232.150.46', '18.208.140.214']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
-
-def do_pack():
-    try:
-        if not os.path.exists("versions"):
-            local("mkdir versions")
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(archive_path))
-        return archive_path
-    except:
-        return None
-
-
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not exists(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
+    if not os.path.exists(archive_path):
         return False
-
+    
     try:
-        put(archive_path, "/tmp/")
-        filename = archive_path.split("/")[-1].split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}\
-            /".format(filename, filename))
-        run("rm /tmp/{}.tgz".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(filename, filename))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current".format(filename))
-        run("touch /data/web_static/current/0-index.html")
-        return True
+        # get archive file name without extension
+        filename_ext = os.path.basename(archive_path)
+        filename = os.path.splitext(filename_ext)[0]
 
-    except:
+        # create paths
+        releases_path = "/data/web_static/releases/{}/".format(filename)
+        tmp_path = "/tmp/{}".format(filename_ext)
+
+        # upload archive to the /tmp/ directory of the web server
+        put(archive_path, tmp_path)
+
+        # create directory to uncompress archive
+        run("mkdir -p {}".format(releases_path))
+
+        # uncompress archive to directory
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+
+        # delete archive file from web server
+        run("rm {}".format(tmp_path))
+
+        # move files to web root
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+
+        # remove web_static directory
+        run("rm -rf {}web_static".format(releases_path))
+
+        # create missing directories
+        run("mkdir -p /data/web_static/releases/{}/public".format(filename))
+        run("mkdir -p /data/web_static/releases/{}/data".format(filename))
+
+        # create missing index.html file
+        run("echo 'Holberton School' | sudo tee /data/web_static/releases/{}/public/index.html".format(filename))
+
+        # delete symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # create new symbolic link to new version of code
+        run("ln -s {} /data/web_static/current".format(releases_path))
+
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        print("Deployment failed: {}".format(str(e)))
         return False
