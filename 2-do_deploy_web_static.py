7253cfcea13bceab89f7986c01850392d29b5c74
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-10 20:01:13 +0000

    Changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 from fabric.api import *
-from os.path import exists
 from datetime import datetime
-
 import os
-
-from fabric.api import *
-import shlex
-from datetime import datetime
-
 
 env.hosts = ['18.232.150.46', '18.208.140.214']
 env.user = 'ubuntu'
+
+
+def do_pack():
+    """
+    Compress files into a .tgz archive
+    """
+
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
 
 
 def do_deploy(archive_path):
@@ -22,13 +31,8 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
+        name = archive_path.split("/")[-1]
+        wname = name.split(".")[0]
 
         releases_path = "/data/web_static/releases/{}/".format(wname)
         tmp_path = "/tmp/{}".format(name)
@@ -41,12 +45,19 @@ def do_deploy(archive_path):
         run("rm -rf {}web_static".format(releases_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(releases_path))
-
-        # Add the required file with the correct content
-        run("mkdir -p /data/web_static/releases/{}/{}".format(wname, "hbnb_static"))
-        run("echo 'Holberton School' | sudo tee /data/web_static/releases/{}/hbnb_static/0-index.html".format(wname))
-
-        print("New version deployed!")
+        run("mv /data/web_static/releases/{}/web_static/hbnb_static/* \
+             /data/web_static/releases/{}/".format(wname, wname))
         return True
     except:
         return False
+
+
+def deploy():
+    """
+    Creates and distributes an archive to the web servers
+    """
+
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
