eb4f7b6a72584a411c6571b5ecb5894b9dfc052e
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-10 21:15:43 +0000

    Changes

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from fabric.api import *
+from os.path import exists
 from datetime import datetime
+
 import os
 
 env.hosts = ['18.232.150.46', '18.208.140.214']
@@ -8,23 +10,28 @@ env.user = 'ubuntu'
 
 
 def do_pack():
-    """Create a compressed archive of web_static contents"""
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+
     try:
         if not os.path.exists("versions"):
             local("mkdir versions")
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
         archive_path = "versions/web_static_{}.tgz".format(timestamp)
-        local("tar -cvzf {} web_static".format(archive_path))
+        local("tar -czvf {} web_static".format(archive_path))
         return archive_path
     except:
         return None
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
+    """
+    Distributes an archive to the web servers
+    """
+
     if not os.path.exists(archive_path):
         return False
-
     try:
         name = archive_path.replace('/', ' ')
         name = shlex.split(name)
@@ -45,15 +52,19 @@ def do_deploy(archive_path):
         run("rm -rf {}web_static".format(releases_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(releases_path))
+        run("mv /data/web_static/current/index.html /data/web_static/current/0-index.html")
+        print("New version deployed!")
         return True
     except:
         return False
 
 
 def deploy():
-    """Create and distribute an archive to the web servers"""
+    """
+    Creates and distributes an archive to the web servers
+    """
+
     archive_path = do_pack()
-    if archive_path is None:
+    if not archive_path:
         return False
-    result = do_deploy(archive_path)
-    return result
+    return do_deploy(archive_path)
