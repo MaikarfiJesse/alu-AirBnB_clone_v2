6e7a62048b701cfea49974627312345fffaee456
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-10 19:01:04 +0000

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
 from fabric.api import *
+from os.path import exists
+from datetime import datetime
 
-env.user = 'ubuntu'
 env.hosts = ['18.232.150.46', '18.208.140.214']
-env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_path = "versions/web_static_{}.tgz".format(timestamp)
+        local("tar -cvzf {} web_static".format(archive_path))
+        return archive_path
+    except:
+        return None
+
 
 def do_deploy(archive_path):
-    """Check if archive exists"""
-    if not os.path.exists(archive_path):
+    """Distributes an archive to the web servers"""
+    if not exists(archive_path):
         return False
 
-    # Get the filename of the archive
-    filename = os.path.basename(archive_path)
-    # Get the name of the file without extension
-    name = os.path.splitext(filename)[0]
-
     try:
-        # Upload the archive to /tmp/ directory of web server
-        put(archive_path, '/tmp/')
-
-        # Create the directory to uncompress the archive
-        run('mkdir -p /data/web_static/releases/{}/'.format(name))
-
-        # Uncompress the archive to the directory
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, name))
-
-        # Delete the archive from web server
-        run('rm /tmp/{}'.format(filename))
-
-        # Move files to the web root
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(name, name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(name))
-
-        # Delete the old symbolic link
-        run('rm -f /data/web_static/current')
-
-        # Create a new symbolic link to the new version of your code
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name))
-
-        # Create a new file at /data/web_static/current/0-index.html
-        sudo('echo "<html><head><title>Testing</title></head><body><h1>Hello, world!</h1></body></html>" > /data/web_static/current/0-index.html')
-
-        print("New version deployed!")
+        put(archive_path, "/tmp/")
+        filename = archive_path.split("/")[-1].split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}\
+            /".format(filename, filename))
+        run("rm /tmp/{}.tgz".format(filename))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(filename))
+        run("sudo sed -i 's|/data/web_static/current/|/data/web_static/current/{}/|g' \
+            /etc/nginx/sites-available/default".format(filename))
+        run("sudo service nginx restart")
         return True
+
     except:
         return False
